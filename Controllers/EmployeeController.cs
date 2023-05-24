f81f06c0a6dc08b6a0f0c0cc9038b84ba45f47fd
BUESA\bartusch <d.bartusch@bueltel.com>
2023-05-24 16:32:52 +0200

    Initial Commit

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeReader.Repositories;
 using EmployeeReader.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EmployeeReader.Controllers
 {
@@ -15,37 +16,80 @@ namespace EmployeeReader.Controllers
       _repository = repository;
     }
 
-    
+
     [HttpGet]
-    public async Task<ActionResult<List<Employee>>> GetAll()
+    public async Task<ActionResult<List<Employee>>> Read()
     {
+      try
+      {
         var employees = await _repository.GetAllAsync();
-        return employees;
+        if (employees == null)
+        {
+          return NoContent();
+        }
+        return Ok(employees);
+      }
+      catch (Exception ex)
+      {
+        return BadRequest(ex.Message);
+      }
+
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Employee>> GetById(int id)
+    public async Task<ActionResult<Employee>> Read(int id)
     {
-      var employee = await _repository.GetByIdAsync(id);
-      return employee;
+      try
+      {
+        var employee = await _repository.GetByIdAsync(id);
+        if (employee == null)
+        {
+          return NoContent();
+        }
+        return Ok(employee);
+        }
+	    catch (System.Exception ex)
+      {
+        return BadRequest(ex.Message);
+      }
     }
 
     [HttpPost]
     public async Task<ActionResult<Employee>> Create(Employee employee)
     {
-      await _repository.CreateAsync(employee);
-      return employee;
-    }
-
-    [HttpPut("{id}")]
-    public async Task<ActionResult<Employee>> Update(int id, Employee employee)
-    {
-      if (id != employee.id)
+      try
       {
-        return BadRequest();
+        await _repository.CreateAsync(employee);
+        if (employee == null)
+        {
+          return NoContent();
+        }
+        return Ok(employee);
       }
-      await _repository.UpdateAsync(employee);
-      return employee;
+      catch (System.Exception ex)
+      {
+        return BadRequest(ex.Message);
+      }
+
+    }
+    [Authorize]
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Employee>> Update(Employee employee)
+    {
+      try
+      {
+        await _repository.UpdateAsync(employee);
+        if (employee == null) 
+        { 
+          return NoContent();
+        }
+        return Ok(employee);
+      }
+      catch (Exception ex)
+      {
+        return BadRequest(ex.Message);
+      }
+
     }
 
     [HttpDelete("{id}")]
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using EmployeeReader.API;
 using EmployeeReader.Repositories;
+using EmployeeReader.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +11,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+  c.SwaggerDoc("v1", new OpenApiInfo { Title = "BasicAuth", Version = "v1" });
+  c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+  {
+    Name = "Authorization",
+    Type = SecuritySchemeType.Http,
+    Scheme = "basic",
+    In = ParameterLocation.Header,
+    Description = "Basic Authorization header using the Bearer scheme."
+  });
+  c.AddSecurityRequirement(new OpenApiSecurityRequirement
+  {
+    {
+      new OpenApiSecurityScheme
+      {
+        Reference = new OpenApiReference
+        {
+          Type = ReferenceType.SecurityScheme,
+          Id = "basic"
+        }
+      },
+      new string[] {}
+    }
+  });
+});
+
+builder.Services.AddAuthentication("BasicAuthentication")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+
+builder.Services.AddScoped<IUserService, UserService>();
 var connectionString = builder.Configuration.GetConnectionString("PgSQL");
 builder.Services.AddScoped<EmployeeRepository>(provider =>
 {
